5f7a30bf27bb7f51d057806eff78a0880d4bed68
devFarjanur <farjanur15-2956@diu.edu.bd>
2026-02-14 13:37:54 +0600

    laravel React Setup

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,24 +1,27 @@
-import './bootstrap';
-import '../css/app.css';
-import '../css/satoshi.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import { BrowserRouter as Router } from 'react-router-dom';
-import { createInertiaApp } from '@inertiajs/react';
-import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from "react-router-dom";
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
 
 createInertiaApp({
-    resolve: name => {
-        const customerPages = import.meta.glob('./Customer/**/*.jsx', { eager: true });
-        const adminPages = import.meta.glob('./Admin/**/*.jsx', { eager: true });
+    resolve: (name) => {
+        const customerPages = import.meta.glob("./customer/**/*.jsx", {
+            eager: true,
+        });
+        const adminPages = import.meta.glob("./admin/**/*.jsx", {
+            eager: true,
+        });
 
         const pages = { ...customerPages, ...adminPages };
-        return pages[`./Customer/${name}.jsx`] || pages[`./Admin/${name}.jsx`];
+        return pages[`./customer/${name}.jsx`] || pages[`./admin/${name}.jsx`];
     },
     setup({ el, App, props }) {
         createRoot(el).render(
             <Router>
                 <App {...props} />
-            </Router>
+            </Router>,
         );
     },
 });
---
+++ resources/js/components/Partials/Layout.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Header from "./Headers/Header";
+import Footer from "./Footers/Footer";
+import DiscountBanner from "./../Home/DiscountBanner";
+import Drawer from "./../Mobile/Drawer/index";
+
+export default function Layout({ children, childrenClasses }) {
+    const [drawer, setDrawer] = useState(false);
+    return (
+        <>
+            <Drawer open={drawer} action={() => setDrawer(!drawer)} />
+            <div className="w-full overflow-x-hidden">
+                <Header drawerAction={() => setDrawer(!drawer)} />
+                <div
+                    className={`w-full  ${childrenClasses || "pt-[30px] pb-[60px]"}`}
+                >
+                    {children && children}
+                </div>
+                <DiscountBanner />
+                <Footer />
+            </div>
+        </>
+    );
+}
